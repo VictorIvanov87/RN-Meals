ed99414866d3ed1d3a52db84e4c61bc62cf9286c
Victor Hristov <victor.hristov@mentormate.com>
2023-10-19 17:41:19 +0300

    Add styles

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,15 +1,51 @@
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
-const CategoryGridTile = ({ categoryTitle, color }) => {
-	return (
-		<View>
-			<Pressable>
-				<View>
-					<Text>{categoryTitle}</Text>
-				</View>
-			</Pressable>
-		</View>
-	);
+const CategoryGridTile = ({ title, color }) => {
+  return (
+    <View style={styles.gridItem}>
+      <Pressable
+        style={({ pressed }) => [styles.button, pressed ? styles.buttonPressed : null]}
+        android_ripple={{ color: "#cccccc" }}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    flex: 1
+  },
+  buttonPressed: {
+    opacity: 0.25
+  },
+  gridItem: {
+    backgroundColor: "white",
+    borderRadius: 8,
+    elevation: 4,
+    flex: 1,
+    height: 150,
+    margin: 16,
+    overflow: "hidden",
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8
+  },
+  innerContainer: {
+    alignItems: "center",
+    borderRadius: 8,
+    flex: 1,
+    justifyContent: "center",
+    padding: 16
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold"
+  }
+});
 
 export default CategoryGridTile;
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,26 +1,23 @@
-import { View, Text, FlatList } from "react-native";
+import { View, FlatList } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
 import CategoryGridTile from "../components/CategoryGridTile";
 
 const renderCategoryItem = (itemData) => {
-	return (
-		<CategoryGridTile
-			categoryTitle={itemData.item.title}
-			color={itemData.item.color}
-		/>
-	);
+  return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />;
 };
 
 const CategoriesScreen = () => {
-	return (
-		<View>
-			<FlatList
-				data={CATEGORIES}
-				renderItem={renderCategoryItem}
-				keyExtractor={(item) => item.id}
-			/>
-		</View>
-	);
+  return (
+    <View>
+      <FlatList
+        data={CATEGORIES}
+        renderItem={renderCategoryItem}
+        numColumns={2}
+        key="categories"
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
 };
 
 export default CategoriesScreen;
